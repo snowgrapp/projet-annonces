65fc6b89a818f14f9bcc5ff0297516e3ba66970e
maxence <129450922+snowgrapp@users.noreply.github.com>
2023-06-15 12:16:24 +0200

    Update index.php
    test micro

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
             <hr class='my-4'>
             <p>Cliquer sur un des boutons ci-dessous pour obtenir une liste des membres ou des types d'abos</p>
             <p class='lead'>
-                <a class='btn btn-primary btn-lg' href='membres.php' role='button'>Membres </a>
+                <a class='btn btn-primary btn-lg' href='membres.php' role='button'>Membres ;)</a>
                 <a class='btn btn-primary btn-lg' href='categories.php' role='button'>Categories </a>
                 <a class='btn btn-primary btn-lg' href='annonces.php' role='button'>Annonces </a>
                 <a class='btn btn-primary btn-lg' href='connexion.php' role='button'>Connexion </a>
